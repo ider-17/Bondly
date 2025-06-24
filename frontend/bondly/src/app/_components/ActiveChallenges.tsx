622a73135f0839b99ae7f0c6d04639033f7bdca9
ider-17 <ider7037@gmail.com>
2025-06-24 13:13:49 +0800

    jlk

--- frontend/bondly/src/app/_components/ActiveChallenges.tsx
+++ frontend/bondly/src/app/_components/ActiveChallenges.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, Mountain, Sparkles } from "lucide-react";
+import { ArrowRight, Mountain, NotebookPen, Sparkles } from "lucide-react";
 
 export default function ActiveChallenges() {
     return (
@@ -27,7 +27,57 @@ export default function ActiveChallenges() {
                     <p className="text-sm font-medium">Өөрийгөө багтаа танилцуулаарай😊</p>
                 </div>
 
-                <div className="flex gap-3"></div>
+                <div className="flex gap-3">
+                    <button className="rounded-full py-1 px-[10px] bg-gray-100 text-xs font-semibold">Week 1</button>
+                    <button className="rounded-full py-1 px-[10px] bg-green-200 text-green-700 text-xs font-semibold">Easy</button>
+                </div>
+
+                <div className="flex gap-2 border border-neutral-300 py-2 px-3 bg-white rounded-lg items-center w-fit">
+                    <p>Write note</p>
+                    <NotebookPen size={20} color="black" />
+                </div>
+            </div>
+
+            <div className="py-[10px] space-y-4">
+                <hr />
+
+                <div className="flex gap-2 items-center">
+                    <div className="w-8 h-8 bg-green-100 rounded-lg flex items-center justify-center">
+                        <Sparkles size={18} color="#22C55E" />
+                    </div>
+                    <p className="text-sm font-medium">Эхний долоо хоногийн сэтгэгдлээ тэмдэглээрэй📝</p>
+                </div>
+
+                <div className="flex gap-3">
+                    <button className="rounded-full py-1 px-[10px] bg-gray-100 text-xs font-semibold">Week 1</button>
+                    <button className="rounded-full py-1 px-[10px] bg-green-200 text-green-700 text-xs font-semibold">Easy</button>
+                </div>
+
+                <div className="flex gap-2 border border-neutral-300 py-2 px-3 bg-white rounded-lg items-center w-fit">
+                    <p>Write note</p>
+                    <NotebookPen size={20} color="black" />
+                </div>
+            </div>
+
+            <div className="py-[10px] space-y-4">
+                <hr />
+
+                <div className="flex gap-2 items-center">
+                    <div className="w-8 h-8 bg-green-100 rounded-lg flex items-center justify-center">
+                        <Sparkles size={18} color="#22C55E" />
+                    </div>
+                    <p className="text-sm font-medium">Компанийн mission statement-ийг уншаарай🤓</p>
+                </div>
+
+                <div className="flex gap-3">
+                    <button className="rounded-full py-1 px-[10px] bg-gray-100 text-xs font-semibold">Week 1</button>
+                    <button className="rounded-full py-1 px-[10px] bg-green-200 text-green-700 text-xs font-semibold">Easy</button>
+                </div>
+
+                <div className="flex gap-2 border border-neutral-300 py-2 px-3 bg-white rounded-lg items-center w-fit">
+                    <p>Write note</p>
+                    <NotebookPen size={20} color="black" />
+                </div>
             </div>
         </div>
     )
